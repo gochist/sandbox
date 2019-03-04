7afbcd1fd169e893bd2a5d98250b3a8b69772bb2
June Yi <june.yi@samsung.com>
2019-03-04 13:19:53 +0900

    Add bionic (ubuntu 18.04)

--- tool/define-vm-linux.sh
+++ tool/define-vm-linux.sh
@@ -20,6 +20,7 @@ if [[ ! "$#" -eq 2 ]]; then
     echo "ex) sudo bash ${0} xenial test"
     echo "images:"
     echo "  - xenial (ubuntu 16.04)"
+    echo "  - bionic (ubuntu 18.04)"
     echo "  - coreos"
     exit 1
 else
@@ -28,6 +29,8 @@ else
 
     if [[ ${IMAGE_NAME} == "xenial" ]]; then
         IMAGE_SOURCE_PATH=${IMAGE_XENIAL_PATH}
+    elif [[ ${IMAGE_NAME} == "bionic" ]]; then
+        IMAGE_SOURCE_PATH=${IMAGE_BIONIC_PATH}
     elif [[ ${IMAGE_NAME} == "coreos" ]]; then
         IMAGE_SOURCE_PATH=${IMAGE_COREOS_PATH}
     else
